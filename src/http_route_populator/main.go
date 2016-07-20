4bcb2c7373099bf34ec5a810626a615be319c88c
Edwin Xie <exie@pivotal.io>
2016-07-20 11:39:06 -0700

    Added HTTP Route Populator for registering routes through NATS
    [#118792447]
    
    Signed-off-by: Mark St.Godard <markstgodard@gmail.com>

---
+++ src/http_route_populator/main.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"http_route_populator/publisher"
+	"http_route_populator/runner"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/nats-io/nats"
+)
+
+var natsEndpoint = flag.String(
+	"nats",
+	"",
+	"Endpoint of the NATS server (including scheme, credentials, and port)",
+)
+
+var backendHost = flag.String(
+	"backendHost",
+	"",
+	"Host for the destination of the route.",
+)
+
+var backendPort = flag.Int(
+	"backendPort",
+	0,
+	"Port for the destination of the route.",
+)
+
+var appDomain = flag.String(
+	"appDomain",
+	"",
+	"The domain name for the routes to register.",
+)
+
+var appName = flag.String(
+	"appName",
+	"",
+	"The name of the app for the route to register.",
+)
+
+var numRoutes = flag.Int(
+	"numRoutes",
+	0,
+	"Number of routes to populate the routing table with.",
+)
+
+var heartbeatInterval = flag.Int(
+	"heartbeatInterval",
+	60,
+	"Time (in seconds) between sending routes.",
+)
+
+func main() {
+	checkRequiredFields()
+	startPopulatingRoutes()
+}
+
+func checkRequiredFields() {
+	flag.Parse()
+	checkFailed := false
+
+	if *natsEndpoint == "" {
+		fmt.Fprintf(os.Stderr, "-nats must be provided\n")
+		checkFailed = true
+	}
+
+	if *backendHost == "" {
+		fmt.Fprintf(os.Stderr, "-backendHost must be provided\n")
+		checkFailed = true
+	}
+
+	if *backendPort <= 0 {
+		fmt.Fprintf(os.Stderr, "-backendPort must be provided\n")
+		checkFailed = true
+	}
+
+	if *appDomain == "" {
+		fmt.Fprintf(os.Stderr, "-appDomain must be provided\n")
+		checkFailed = true
+	}
+
+	if *appName == "" {
+		fmt.Fprintf(os.Stderr, "-appName must be provided\n")
+		checkFailed = true
+	}
+
+	if *numRoutes <= 0 {
+		fmt.Fprintf(os.Stderr, "-numRoutes must be provided\n")
+		checkFailed = true
+	}
+
+	if *heartbeatInterval <= 0 {
+		fmt.Fprintf(os.Stderr, "-heartbeatInterval must be greater than 0\n")
+		checkFailed = true
+	}
+
+	if checkFailed {
+		fmt.Fprintf(os.Stderr, "\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+}
+
+func startPopulatingRoutes() {
+	job := publisher.Job{
+		PublishingEndpoint: *natsEndpoint,
+
+		BackendHost: *backendHost,
+		BackendPort: *backendPort,
+
+		AppDomain: *appDomain,
+		AppName:   *appName,
+
+		StartRange: 0,
+		EndRange:   *numRoutes,
+	}
+
+	numCPU := 1 // runtime.NumCPU()
+	// Heuristic to avoid spawning more goroutines than needed
+	if *numRoutes < 1000 {
+		numCPU = 1
+	}
+	interval := time.Duration(*heartbeatInterval) * time.Second
+	r := runner.NewRunner(createNATSConnection, job, numCPU, interval)
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	// Stop the runner if we receive an interrupt
+	go func() {
+		sig := <-sigs
+		fmt.Fprintf(os.Stderr, "Received signal %s\n", sig)
+		r.Stop()
+	}()
+
+	err := r.Start()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error starting runner: %s\n", err)
+	}
+	err = r.Wait()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error running: %s\n", err)
+	}
+}
+
+func createNATSConnection(endpoint string) (publisher.PublishingConnection, error) {
+	nc, err := nats.Connect(endpoint)
+	if err != nil {
+		return nil, err
+	}
+	return nc, nil
+}
---
+++ src/http_route_populator/publisher/publisher.go
@@ -0,0 +1,117 @@
+package publisher
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type ConnectionCreator func(endpoint string) (PublishingConnection, error)
+
+//go:generate counterfeiter -o fakes/fake_publishing_connection.go . PublishingConnection
+type PublishingConnection interface {
+	Publish(subj string, data []byte) error
+	Close()
+}
+
+type Job struct {
+	PublishingEndpoint string
+
+	BackendHost string
+	BackendPort int
+
+	AppDomain  string
+	AppName    string
+	StartRange int
+	EndRange   int
+}
+
+func (j Job) validate() error {
+	validationError := func(err string) error {
+		return fmt.Errorf("Invalid job properties: %s", err)
+	}
+	if j.PublishingEndpoint == "" {
+		return validationError(`Missing "AppDomain"`)
+	}
+
+	if j.BackendHost == "" {
+		return validationError(`Missing "BackendHost"`)
+	}
+
+	if j.BackendPort == 0 {
+		return validationError(`Missing "BackendPort"`)
+	}
+
+	if j.AppDomain == "" {
+		return validationError(`Missing "AppDomain"`)
+	}
+
+	if j.AppName == "" {
+		return validationError(`Missing "AppName"`)
+	}
+
+	if j.EndRange <= j.StartRange {
+		return validationError(`Invalid route "StartRange" and "EndRange"`)
+	}
+	return nil
+}
+
+type RouteData struct {
+	Host string   `json:"host"`
+	Port int      `json:"port"`
+	URIs []string `json:"uris"`
+}
+
+type Publisher struct {
+	job  Job
+	conn PublishingConnection
+
+	data [][]byte
+}
+
+func NewPublisher(job Job) *Publisher {
+	return &Publisher{
+		job: job,
+	}
+}
+
+func (p *Publisher) Initialize(c ConnectionCreator) error {
+	err := p.job.validate()
+	if err != nil {
+		return err
+	}
+
+	conn, err := c(p.job.PublishingEndpoint)
+	p.conn = conn
+	if err != nil {
+		return err
+	}
+
+	routeData := RouteData{
+		Host: p.job.BackendHost,
+		Port: p.job.BackendPort,
+	}
+
+	for i := p.job.StartRange; i < p.job.EndRange; i += 1 {
+		routeData.URIs = []string{fmt.Sprintf("%s-%d.%s", p.job.AppName, i, p.job.AppDomain)}
+		marshaledData, err := json.Marshal(routeData)
+		if err != nil {
+			return err
+		}
+		p.data = append(p.data, marshaledData)
+	}
+	return nil
+}
+
+func (p *Publisher) PublishRouteRegistrations() error {
+	for i := 0; i < (p.job.EndRange - p.job.StartRange); i += 1 {
+		err := p.conn.Publish("router.register", p.data[i])
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (p *Publisher) Finish() {
+	p.conn.Close()
+}
