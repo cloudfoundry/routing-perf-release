651aebecce077b3400a26fbf496449b23c486ce0
Shash Reddy <sreddy@pivotal.io>
2016-12-15 12:14:17 -0800

    Add integration tests to cpumonitor
    [#135887919]
    
    Signed-off-by: Nino Kodabande <ninok@hpe.com>

--- src/cpumonitor/cpumonitor.go
+++ src/cpumonitor/cpumonitor.go
@@ -14,21 +14,21 @@ import (
 // security??
 
 var (
-	port        = flag.Int("port", 0, "port for the server")
-	runInterval = flag.Int("runInterval", 1, "Sampling interval, how ofter to call CPU stats in second")
+	port        = flag.Int("port", 8080, "port for the server")
+	runInterval = flag.Int("runInterval", 1000, "Sampling interval, how ofter to call CPU stats in millisecond")
 	cpuInterval = flag.Int("cpuInterval", 0, "Time between each CPU stat in second, if 0 is given it will compare the current cpu times against the last call")
 	perCPU      = flag.Bool("perCpu", false, "Percent calculates the percentage of cpu used either per CPU or combined")
 )
 
 func main() {
 	flag.Parse()
-	if *port == 0 {
-		fmt.Fprintf(os.Stderr, "-port must be provided")
+	if *port == 0 || *port > 65535 {
+		fmt.Fprintf(os.Stderr, "-port must be within range 1024-65535")
 		os.Exit(1)
 
 	}
-	if *port > 65535 {
-		fmt.Fprintf(os.Stderr, "-port must be valid")
+	if *runInterval < 1 {
+		fmt.Fprintf(os.Stderr, "-runInterval must be above 1")
 		os.Exit(1)
 
 	}
@@ -37,8 +37,9 @@ func main() {
 }
 
 func startServer() {
+	log.Printf("CPU monitor is running on %d \n", *port)
 	cpuCalculator := new(stats.CPUOps)
-	runInterval := time.Duration(int32(*runInterval)) * time.Second
+	runInterval := time.Duration(int32(*runInterval)) * time.Millisecond
 	cpuInterval := time.Duration(int32(*cpuInterval)) * time.Second
 	cpuCollector := stats.NewCPUCollector(cpuCalculator, stats.DefaultTicker(), runInterval, cpuInterval, *perCPU)
 	statsHandler := stats.NewStatHandler(cpuCollector)
--- src/cpumonitor/cpumonitor_test.go
+++ src/cpumonitor/cpumonitor_test.go
@@ -1,8 +1,12 @@
 package main_test
 
 import (
+	"cpumonitor/stats"
+	"io/ioutil"
+	"net/http"
 	"os/exec"
 	"strconv"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -23,17 +27,78 @@ var runCpumonitor = func(port, runInterval, cpuInterval int, perCpu bool) *gexec
 }
 
 var _ = Describe("Cpumonitor", func() {
+	var session *gexec.Session
+	AfterEach(func() {
+		session.Kill()
+	})
 	Context("Argument handling", func() {
 		It("errors if no port number is passed", func() {
-			session := runCpumonitor(0, 0, 0, false)
+			session = runCpumonitor(0, 0, 0, false)
 			Eventually(session).Should(gexec.Exit(1))
-			Expect(session.Err).To(gbytes.Say("-port must be provided"))
+			Expect(session.Err).To(gbytes.Say("-port must be within range 1024-65535"))
 		})
 
 		It("errors if port number is not valid", func() {
-			session := runCpumonitor(65536, 0, 0, false)
+			session = runCpumonitor(65536, 0, 0, false)
 			Eventually(session).Should(gexec.Exit(1))
-			Expect(session.Err).To(gbytes.Say("-port must be valid"))
+			Expect(session.Err).To(gbytes.Say("-port must be within range 1024-65535"))
+		})
+
+		It("errors if runtInterval is less than 1", func() {
+			session = runCpumonitor(6553, 0, 0, false)
+			Eventually(session).Should(gexec.Exit(1))
+			Expect(session.Err).To(gbytes.Say("-runInterval must be above 1"))
+		})
+	})
+	Context("when port and runInterval is passed", func() {
+		BeforeEach(func() {
+			session = runCpumonitor(6530, 100, 1, false)
+			//wait for server to collect stats
+			time.Sleep(time.Second * 1)
+
+			Eventually(session.Err).Should(gbytes.Say("CPU monitor is running on 6530"))
+		})
+
+		It("returns an http error if start is called multiple times", func() {
+			resp, err := http.Get("http://localhost:6530/start")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(resp.StatusCode).To(Equal(http.StatusOK))
+			resp, err = http.Get("http://localhost:6530/start")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(resp.StatusCode).To(Equal(http.StatusBadRequest))
+			body, err := ioutil.ReadAll(resp.Body)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(string(body)).To(ContainSubstring(stats.ErrMultipleCollector.Error()))
+
+		})
+
+		It("returns http error if stop called without calling start", func() {
+			var resp *http.Response
+			var err error
+			resp, err = http.Get("http://localhost:6530/stop")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(resp.StatusCode).To(Equal(http.StatusBadRequest))
+			var body []byte
+			body, err = ioutil.ReadAll(resp.Body)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(string(body)).To(ContainSubstring(stats.ErrCollectorNotRunning.Error()))
+		})
+
+		It("returns CPU stats", func() {
+			resp, err := http.Get("http://localhost:6530/start")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(resp.StatusCode).To(Equal(http.StatusOK))
+
+			resp, err = http.Get("http://localhost:6530/stop")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resp.StatusCode).To(Equal(http.StatusOK))
+
+			body, err := ioutil.ReadAll(resp.Body)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(string(body)).To(ContainSubstring("Percentage"))
+			Expect(string(body)).To(ContainSubstring("TimeStamp"))
+			session.Kill()
+
 		})
 	})
 })
